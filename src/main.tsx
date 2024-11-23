1382eb0161c61652e0639e942bdb9724f682d9e3
daniel-judit <daniel.resende@judit.io>
2024-11-23 12:45:35 -0300

    feat: chakra ui added

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './pages/App.tsx';
+import { Provider } from "@/components/ui/provider"
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./pages/App"
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <Provider>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+)
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,8 +1,20 @@
 import { Fragment } from 'react';
-import './App.css';
-
+import { Button } from "@/components/ui/button"
+import { HStack } from '@chakra-ui/react';
 function App() {
-  return <Fragment></Fragment>;
+  return (
+  <Fragment>
+    <div className="container mx-auto">
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <HStack>
+      <Button>teste</Button>
+    </HStack>
+    </div>
+  </Fragment>
+ 
+  )
 }
 
 export default App;
